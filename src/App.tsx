c2257d5eb8d9e40f9d81683494b37762937416f3
leotex26 <160853319+leotex26@users.noreply.github.com>
2026-08-21 17:51:19 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Shadow Hunt</h1>
+      <h2>Carte : Bellevue</h2>
+
+      <p>Le prototype de la carte sera affiché ici.</p>
+    </main>
+  );
+}
+
+export default App;
